b9186fa795831155d5c7491b53fe6d3f3ec637c0
caohao0730 <caohao0730@163.com>
2018-12-25 19:16:24 +0800

    Update restart.sh

--- openresty/openresty-pool/restart.sh
+++ openresty/openresty-pool/restart.sh
@@ -3,4 +3,4 @@ echo $pid
 kill -9 $pid
 rm -rf /tmp/redis_pool.sock
 rm -rf /tmp/mysql_pool.sock
-/usr/local/openresty.1.13/nginx/sbin/nginx -p ~/openresty/openresty-pool
+/usr/local/openresty.1.13/nginx/sbin/nginx -p ~/openresty-pool
